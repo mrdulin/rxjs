6da73cce94a08e00900351e362386dd03663f4b6
mrdulin <novaline@qq.com>
2017-08-20 13:31:32 +0800

    update

---
+++ src/demos-rxjs-v5/throw-and-catch-an-exception/catch-and-stop.ts
@@ -0,0 +1,27 @@
+import * as Rx from 'rxjs';
+
+const interval$ = Rx.Observable.interval(1000);
+
+interval$
+  .map((val) => {
+    if (val > 5) {
+      throw new Error('error');
+    }
+    return val;
+  })
+  .catch((err, source) => {
+    // return Rx.Observable.empty();
+    return Rx.Observable.throw(err);
+  })
+  .subscribe(
+  (val) => {
+    console.log(val);
+  },
+  (err) => {
+    console.log('error happened', err);
+  },
+  () => {
+    console.log('complete');
+  }
+  );
+
