53c3219ec3519aa3ef3e50c8973811633aebbcdd
mrdulin <novaline@qq.com>
2017-09-28 15:00:22 +0800

    添加异常处理

--- src/demos-rxjs-v5/convert-promise-to-observable/index.ts
+++ src/demos-rxjs-v5/convert-promise-to-observable/index.ts
@@ -1,13 +1,22 @@
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromPromise';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/throw';
+
 import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/catch';
 
 //缓存数据
 const getDataFromCache = (cacheMock: boolean) =>
   new Promise((resolve) => setTimeout(resolve, 1000, cacheMock ? 'data from cache' : null));
 //接口请求数据
-const getDataFromServer = () => Observable.of('data from server');
+const getDataFromServer = () => {
+  if (Math.random() > 0.5) {
+    return Observable.of('data from server');
+  } else {
+    return Observable.throw('server error');
+  }
+};
 
 /**
  * 如果有缓存数据，从缓存数据中取
@@ -26,11 +35,14 @@ function main() {
       return Observable.of(cacheData);
     }
     return getDataFromServer();
-  });
+  })
+  .catch((err) => Observable.throw('请求数据失败'));
 }
 
 
 main().subscribe((data) => {
   console.log('data:', data);
+}, (errMsg: string) => {
+  console.log('errMsg: ', errMsg);
 });
 
